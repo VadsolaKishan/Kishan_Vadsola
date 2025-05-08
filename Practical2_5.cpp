f0e92348717ddb4fd5ff723f1916da2b00637363
VadsolaKishan <vadsolakishan1310@gmail.com>
2025-05-08 08:56:53 +0530

    Update Practical2_5.cpp

--- Practical2_5.cpp
+++ Practical2_5.cpp
@@ -1,3 +1,21 @@
+/* A regional banking institution sought to improve its loan management process by developing a system 
+that could efficiently handle loan details for applicants. The system was expected to streamline the 
+calculation of monthly EMIs (Equated Monthly Instalments) and provide detailed loan summaries for 
+customers. This initiative aimed to enhance customer experience by offering accurate and transparent 
+information about their loans. 
+To meet these requirements, the bank's technology team was tasked with designing a solution. They 
+envisioned a system where each loan would be uniquely identified by a loan ID, and additional details 
+such as the applicant's name, total loan amount, annual interest rate, and loan tenure in months 
+would be stored. The team also emphasized the importance of accurately calculating the EMI using a 
+standard formula based on the loan amount, interest rate, and tenure. The formula incorporated 
+compound interest principles to determine the fixed monthly payment for the loan term. 
+EMI= 𝑃 ∗ 𝑅 ∗ (1 + 𝑅)^𝑇 / ((1+𝑅)^𝑇) −1
+ As part of the implementation, the developers planned to initialize loans either with default values for 
+testing purposes or with actual customer data provided at the time of application. The system needed 
+to include a feature to display comprehensive loan details, including the calculated EMI, in a customer
+friendly format. This functionality was intended to aid both customers and bank staff in managing 
+loan-related queries effectively. */
+
 #include<iostream>
 #include<math.h>
 using namespace std;
