4cb49e68ae1fe8d6a78159b3a840ce9de8b65d46
VadsolaKishan <vadsolakishan1310@gmail.com>
2025-05-08 08:52:00 +0530

    Update Practical1_2_1.cpp

--- Practical1_2_1.cpp
+++ Practical1_2_1.cpp
@@ -1,3 +1,21 @@
+/* A small retail store is facing challenges in managing its inventory effectively. The store sells a 
+variety of products, each identified by a unique product ID, a name, the available quantity in 
+stock, and the price per unit. To streamline their operations, the store needs a basic system to 
+manage this inventory efficiently. 
+The system must provide the ability to add new products to the inventory, ensuring that each 
+product has its ID, name, quantity, and price properly recorded. Additionally, the system should 
+allow the store staff to update the quantity of any existing product, such as when new stock 
+arrives or when items are sold. 
+Another essential feature of the system is the calculation of the total value of all products in 
+the inventory, which is determined by multiplying the quantity of each product by its price and 
+summing these values for all products. 
+The store management is exploring two approaches for this system: a procedural approach and 
+an object-oriented approach. The goal is to evaluate these approaches by comparing their ease 
+of implementation, code reusability, and overall complexity. 
+The system's design and implementation should consider these requirements and provide an 
+effective solution (either procedural approach or an object-oriented approach) to the store's 
+inventory management problems. */
+
 #include <iostream>
 using namespace std;
 
